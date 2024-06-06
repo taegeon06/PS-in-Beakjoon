321c2e9bfa8aa1db2e7caccaddb8cb6c114a41fd
taegeon06 <129233335+taegeon06@users.noreply.github.com>
2024-06-06 21:29:48 +0900

    [Silver II] Title: 신나는 함수 실행, Time: 112 ms, Memory: 32140 KB -BaekjoonHub

--- 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.py
+++ 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.py
@@ -1,4 +1,38 @@
-a, b, c = map(int, input().split())
+import sys
+input = sys.stdin.readline
+a, b, c = map(int, input().rstrip().split())
+
+dp = [[[0 for i in range(51)] for j in range(51)] for k in range(51)]
+
+for i in range(21) :
+    for j in range(21) :
+        for k in range(21) :
+            if i <= 0 or j <= 0 or k <= 0 :
+                dp[i][j][k] = 1
+
+            elif i > 20 or j > 20 or k > 20 :
+                dp[i][j][k] = dp[20][20][20]
+
+            elif i < j and j < k :
+                dp[i][j][k] = dp[i][j][k - 1] + dp[i][j - 1][k - 1] - dp[i][j - 1][k]
+
+            else :
+                dp[i][j][k] = dp[i - 1][j][k] + dp[i - 1][j - 1][k] + dp[i - 1][j][k - 1] - dp[i - 1][j - 1][k - 1]
+
+for i in range(51) :
+    for j in range(51) :
+        for k in range(51) :
+            if i <= 0 or j <= 0 or k <= 0 :
+                dp[i][j][k] = 1
+
+            elif i > 20 or j > 20 or k > 20 :
+                dp[i][j][k] = dp[20][20][20]
+
+            elif i < j and j < k :
+                dp[i][j][k] = dp[i][j][k - 1] + dp[i][j - 1][k - 1] - dp[i][j - 1][k]
+
+            else :
+                dp[i][j][k] = dp[i - 1][j][k] + dp[i - 1][j - 1][k] + dp[i - 1][j][k - 1] - dp[i - 1][j - 1][k - 1]
 
 while True :
     if a == -1 and b == -1 and c == -1 :
@@ -8,38 +42,6 @@ while True :
         print('w(%d, %d, %d) = %d' %(a, b, c, 1))
 
     else :
-        dp = [[[0 for i in range(51)] for j in range(51)] for k in range(51)]
-
-        for i in range(21) :
-            for j in range(21) :
-                for k in range(21) :
-                    if i <= 0 or j <= 0 or k <= 0 :
-                        dp[i][j][k] = 1
-
-                    elif i > 20 or j > 20 or k > 20 :
-                        dp[i][j][k] = dp[20][20][20]
-
-                    elif i < j and j < k :
-                        dp[i][j][k] = dp[i][j][k - 1] + dp[i][j - 1][k - 1] - dp[i][j - 1][k]
-
-                    else :
-                        dp[i][j][k] = dp[i - 1][j][k] + dp[i - 1][j - 1][k] + dp[i - 1][j][k - 1] - dp[i - 1][j - 1][k - 1]
-
-        for i in range(a + 1) :
-            for j in range(b + 1) :
-                for k in range(c + 1) :
-                    if i <= 0 or j <= 0 or k <= 0 :
-                        dp[i][j][k] = 1
-
-                    elif i > 20 or j > 20 or k > 20 :
-                        dp[i][j][k] = dp[20][20][20]
-
-                    elif i < j and j < k :
-                        dp[i][j][k] = dp[i][j][k - 1] + dp[i][j - 1][k - 1] - dp[i][j - 1][k]
-
-                    else :
-                        dp[i][j][k] = dp[i - 1][j][k] + dp[i - 1][j - 1][k] + dp[i - 1][j][k - 1] - dp[i - 1][j - 1][k - 1]
-
         print('w(%d, %d, %d) = %d' %(a, b, c, dp[a][b][c]))
 
     a, b, c = map(int, input().split())
