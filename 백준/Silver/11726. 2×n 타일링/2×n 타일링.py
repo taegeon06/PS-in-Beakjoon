71bbf733b57566198f1b38079f31871bbc687683
taegeon06 <129233335+taegeon06@users.noreply.github.com>
2024-06-27 00:25:50 +0900

    [Silver III] Title: 2×n 타일링, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
@@ -1,17 +1,11 @@
 N = int(input())
 
-dp = []
+dp = [0 for i in range(N + 2)]
 
-for i in range(N + 1) :
-    dp.append([0, 0, 0])
-dp.append([0, 0, 0])
-dp[1][1] = 1
-dp[1][2] = 0
-dp[2][1] = 1
-dp[2][2] = 1
+dp[1] = 1
+dp[2] = 2
 
-for i in range(3, N + 1) :
-    dp[i][1] = dp[i - 1][1] + dp[i - 1][2]
-    dp[i][2] = dp[i - 2][1] + dp[i - 2][2]
+for k in range(3, N + 1) :
+    dp[k] = dp[k - 1] + dp[k - 2]
 
-print((dp[N][1] + dp[N][2]) % 10007)
+print(dp[N] % 10007)
