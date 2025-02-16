a4a8ad867fd8ea63287c9840ebea7eadc3ad7ce2
taegeon06 <129233335+taegeon06@users.noreply.github.com>
2025-02-16 18:56:27 +0900

    [Gold II] Title: 불켜기, Time: 624 ms, Memory: 42072 KB -BaekjoonHub

--- 백준/Gold/11967. 불켜기/불켜기.py
+++ 백준/Gold/11967. 불켜기/불켜기.py
@@ -1,6 +1,7 @@
 from collections import deque
 
 qu = deque()
+
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
@@ -11,58 +12,52 @@ def BFS() :
         Y = coo[1]
 
         while li[X][Y] :
-            tm = li[X][Y].pop()
-            rx = tm[0]
-            ry = tm[1]
-            #print('pass')
-            #print(X, Y, rx, ry)
+            rx, ry = li[X][Y].pop()
 
-            if vi[rx][ry] : continue
+            if vi[rx][ry][0] == 1 : continue
             if CHK(rx, ry) :
-                vi[rx][ry] = 1
+                vi[rx][ry][0] = 1
                 qu.appendleft([rx, ry])
-            
-            fr[rx][ry] = 1
+                
+            vi[rx][ry][1] = 1
         
+                    
         for i in range(4) :
             nx = X + dx[i]
             ny = Y + dy[i]
 
             if nx < 0 or nx >= N or ny < 0 or ny >= N : continue
-            if vi[nx][ny] == 1 or fr[nx][ny] == 0 : continue
+            if vi[nx][ny][0] == 1 or vi[nx][ny][1] == 0: continue
 
-            vi[nx][ny] = 1
             qu.appendleft([nx, ny])
+            vi[nx][ny][0] = 1
 
 def CHK(X, Y) :
     for i in range(4) :
-        nx = dx[i] + X
-        ny = dy[i] + Y
-        
+        nx = X + dx[i]
+        ny = Y + dy[i]
+
         if nx < 0 or nx >= N or ny < 0 or ny >= N : continue
-        if vi[nx][ny] == 1 : return True
+        if vi[nx][ny][0] == 1 : return True
     
     return False
 
 N, M = map(int, input().split())
 li = [[deque() for i in range(N)] for j in range(N)]
-vi = [[0 for i in range(N)] for j in range(N)]
-fr = [[0 for i in range(N)] for j in range(N)]
+vi = [[[0, 0] for i in range(N)] for j in range(N)]
 
 for i in range(M) :
     x, y, a, b = map(int, input().split())
     li[x - 1][y - 1].append([a - 1, b - 1])
 
-vi[0][0] = 1
-fr[0][0] = 1
+vi[0][0][0] = 1
+vi[0][0][1] = 1
 qu.appendleft([0, 0])
-
 BFS()
 
-ans = 0
-
+cnt = 0
 for i in range(N) :
     for j in range(N) :
-        ans += fr[i][j]
+        cnt += vi[i][j][1]
 
-print(ans)
+print(cnt)
