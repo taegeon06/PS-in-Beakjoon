ed81ffee50d6b66497649c75aebf7d7c1a19d857
taegeon06 <129233335+taegeon06@users.noreply.github.com>
2025-03-30 08:48:52 +0900

    [Platinum III] Title: 디지털 비디오 디스크(DVDs), Time: 2220 ms, Memory: 216040 KB -BaekjoonHub

---
+++ 백준/Platinum/9345. 디지털 비디오 디스크（DVDs）/디지털 비디오 디스크（DVDs）.py
@@ -0,0 +1,79 @@
+import sys
+
+input = sys.stdin.readline
+
+T = int(input())
+
+def query(i, s, e, l, r) :
+    if s > r or e < l : return 0, n - 1, 0, n - 1
+    if s <= l and e >= r : return tree[i], tree1[i], tree2[i], tree3[i]
+
+    mid = (l + r) // 2
+
+    v1, v11, vv1, vv11 = query(i * 2, s, e, l, mid)
+    v2, v22, vv2, vv22 = query(i * 2 + 1, s, e, mid + 1, r)
+
+    return max(v1, v2), min(v11, v22), max(vv1, vv2), min(vv11, vv22)
+
+def update(i, v, v1) :
+    i += t
+    tree[i] = v
+    tree1[i] = v1
+
+    while i > 1 :
+        i //= 2
+        tree[i] = max(tree[i * 2], tree[i * 2 + 1])
+        tree1[i] = min(tree1[i * 2], tree1[i * 2 + 1])
+
+
+for _ in range(T) :
+    n, k = map(int, input().split())
+
+    t = 1
+
+    while n > t :
+        t *= 2
+
+    tree = [0 for i in range(t * 2)]
+    tree1 = [n - 1 for i in range(t * 2)]
+    tree2 = [0 for i in range(t * 2)]
+    tree3 = [n - 1 for i in range(t * 2)]
+
+    for i in range(n) :
+        tree[i + t] = i
+        tree1[i + t] = i
+        tree2[i + t] = i
+        tree3[i + t] = i
+
+    
+    for i in range(t - 1, 0, -1) :
+        tree[i] = max(tree[i * 2], tree[i * 2 + 1])
+        tree1[i]= min(tree1[i * 2], tree1[i * 2 + 1])
+
+        tree2[i] = max(tree2[i * 2], tree2[i * 2 + 1])
+        tree3[i]= min(tree3[i * 2], tree3[i * 2 + 1])
+    
+    #print(tree, ai)
+
+    for i in range(k) :
+        q, a, b = map(int, input().split())
+
+        if q == 0 :
+            v1 = tree[a + t]
+            v11 = tree1[a + t]
+            v2 = tree[b + t]
+            v22 = tree1[b + t]
+            update(a, v2, v22)
+            update(b, v1, v11)
+        
+        if q == 1 :
+            v1, v2, v11, v22 = query(1, a, b, 0, t - 1)
+
+            if v1 == v11 and v2 == v22 :
+                print("YES")
+            
+            else :
+                print("NO")
+            
+            #print(v1, v2, v11, v22)
+            #print(tree, tree1, tree2, tree3)
