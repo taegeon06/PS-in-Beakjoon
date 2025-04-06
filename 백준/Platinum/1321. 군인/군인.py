d9e3e01fe1ecefb1721663e7bc82fd1304866983
taegeon06 <129233335+taegeon06@users.noreply.github.com>
2025-04-06 10:48:32 +0900

    [Platinum V] Title: 군인, Time: 544 ms, Memory: 191664 KB -BaekjoonHub

--- 백준/Platinum/1321. 군인/군인.py
+++ 백준/Platinum/1321. 군인/군인.py
@@ -8,46 +8,15 @@ while n > t :
     t *= 2
 
 def seg(i, v, s, e) :
-    mid = (s + e) // 2
+    if s == e : return s
 
-    if i * 2 >= t :
-        idx = i * 2
+    m = (s + e) // 2
 
-        v1 = tree[idx] + query(1, 0, idx - t - 1, 0, t - 1)
-        v2 = tree[idx + 1] + query(1, 0, idx - t, 0, t - 1)
+    if tree[i * 2] >= v :
+        return seg(i * 2, v, s, m)
 
-        if v <= v1 : 
-            #print(idx - t, tree[idx], v)
-            return idx - t
-        if v > v1 and v <= v2 : 
-            #print(idx + 1 - t, tree[idx + 1], v)
-            return idx + 1 - t 
-    
     else :
-        v1 = tree[i * 2] + query(1, 0, s - 1, 0, t - 1)
-        v2 = tree[i * 2 + 1] + query(1, 0, mid, 0, t - 1)
-
-
-        #print(v1, v2, tree[i * 2 + 1], v)
-
-        if v <= v1 : 
-            #print(1)
-            return seg(i * 2, v, s, mid)
-
-        if v > v1 and v <= v2 :
-            #print(2)
-            return seg(i * 2 + 1, v, mid + 1, e)
-    
-def query(i, s, e, l, r) :
-    if s > r or e < l : return 0
-    if s <= l and e >= r : return tree[i]
-
-    mid = (l + r) // 2
-
-    v1 = query(i * 2, s, e, l, mid)
-    v2 = query(i * 2 + 1, s, e, mid + 1, r)
-
-    return v1 + v2
+        return seg(i * 2 + 1, v - tree[i * 2], m + 1, e)
 
 
 def update(i, v) :
