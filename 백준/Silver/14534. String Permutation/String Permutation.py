ca7683f4e3434cb48041c304231fd365f713a385
taegeon06 <129233335+taegeon06@users.noreply.github.com>
2025-04-18 16:47:21 +0900

    [Silver IV] Title: String Permutation, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/14534. String Permutation/String Permutation.py
+++ 백준/Silver/14534. String Permutation/String Permutation.py
@@ -1,14 +1,31 @@
-from itertools import permutations
-
 t = int(input())
+
+def back(i, v, s) :
+    if i == v :
+        tm = ''
+        for j in range(len(s)) :
+            tm += a[s[j]] 
+        print(tm)
+        return
+    
+    else :
+        for j in range(v) :
+            f = 0
+            for k in range(len(s)) :
+                if j == s[k] :
+                    f = 1
+                    break
+            
+            if f == 1 : continue
+
+            else :
+                s.append(j)
+                back(i + 1, v, s)
+                s.pop()
 
 for i in range(t) :
     a = list(input())
 
-    v = list(permutations(a, len(a)))
-    
     print('Case # %d:' % (i + 1))
-
-    for i in range(len(v)) :
-        t = ''.join(v[i])
-        print(t)
+    for j in range(len(a)) :
+        back(1, len(a), [j])
